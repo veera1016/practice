ed5e8d4d21278d1dbdaf366e6874eea4d2cc6d4e
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-09 19:02:21 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,19 @@
-output "react_service_url" {
-  value = aws_route53_record.react_record.name
+output "ecs_task_definition_arn" {
+  value = aws_ecs_task_definition.strapi_task_definition.arn
 }
 
-output "strapi_service_url" {
-  value = aws_route53_record.strapi_record.name
+output "ecs_cluster_id" {
+  value = aws_ecs_cluster.strapi_cluster.id
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.strapi_service1.name
+}
+
+output "ecs_service_task_definition" {
+  value = aws_ecs_service.strapi_service1.task_definition
+}
+
+output "route53_dns_name" {
+  value = aws_route53_record.strapi_subdomain.fqdn
 }
